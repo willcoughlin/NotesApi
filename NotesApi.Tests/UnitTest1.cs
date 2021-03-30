7dcb4beb0caeb53d5985cf81ef3e3639b437b47c
Will Coughlin <willcoughlin@gmail.com>
2021-03-30 16:32:40 -0500

    Change solution layout

---
+++ NotesApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NotesApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+    }
+}
